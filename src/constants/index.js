e5c6c0f206e91a3d8881278642d66e63fca2c33f
mohabelgendyy <mohabelgendyy@gmail.com>
2024-01-30 18:54:23 +0200

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,245 @@
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+  c,
+  mysql,
+  php,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: php,
+    name: "php",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: c,
+    name: "c++",
+    type: "Backend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: mysql,
+    name: "mysql",
+    type: "DataBase",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+];
+
+export const experiences = [
+  {
+    title: "React.js Developer",
+
+    iconBg: "#accbe1",
+
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "React Native Developer",
+
+    iconBg: "#fbc3bc",
+
+    points: [
+      "Developing and maintaining applications using React Native and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+
+    iconBg: "#b7e4c7",
+
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+
+    iconBg: "#a2d2ff",
+
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/mohabelgendyy",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/mohabelgendyy/",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Jobs For You",
+    description:
+      "Developed a mobile application that finding jobs on market , helping users find the best jobs.",
+    link: "https://github.com/mohabelgendyy/React_native_jobsApp",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Full Stack MarketPlace Clone",
+    description:
+      'Created a full-stack MobileApp of the popular platform "MarketPlace," enabling users to buy and sell.',
+    link: "https://github.com/mohabelgendyy/marketplace-reactNative",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Car Finding App",
+    description:
+      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
+    link: "https://github.com/mohabelgendyy/car-app-typescript-react-nextjs",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Anime WebApp ",
+    description:
+      "Built a complete webApp Anime with a ServerSideApp and InfintyScroll.",
+    link: "https://github.com/mohabelgendyy/AnimewebApp-Next14-ServerSideApp-InfiniteScroll-FramerMotionAnimations",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Resturant Web Application",
+    description: "Developed a web application for Resturant listings.",
+    link: "https://github.com/mohabelgendyy/website-typescript-nextjs-tailwind-react",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "Ecommerce Application",
+    description: "App that you can find the best product to wear.",
+    link: "https://github.com/mohabelgendyy/Ecommerce-App-ReactNative-Express",
+  },
+];
